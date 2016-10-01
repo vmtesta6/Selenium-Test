2bea5bed1dc5ecf5293ac32a16b4bf16efd01760
sailakshmiv <lakshmi.saiv99@gmail.com>
2016-10-01 13:46:34 +0530

    Update Login.java

--- src/org/bluemeric/com/Login.java
+++ src/org/bluemeric/com/Login.java
@@ -13,7 +13,6 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
-import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
